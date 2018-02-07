3520c5ed4bd51d16976f523d7f1e566e0b2a98c0
1p2m3m <pmezger7@gmail.com>
2018-02-07 12:12:31 +0100

    Initial commit

---
+++ numbergame.rb
@@ -0,0 +1,22 @@
+
+
+puts "Welcome to the Numbers Game! What is your name?"
+name = gets.chomp
+puts "Welcome #{name}"
+
+number = rand(1..10)
+puts "Please guess a number between 1 -10"
+
+winner = false
+while winner == false
+  guess = gets.chomp.to_i
+
+if guess > number then
+    puts "Sorry, It's too high"
+    elsif guess < number then
+    puts "Sorry, It's too low"
+    else
+    puts "#{name} you are correct"
+    winner = true
+  end
+end
