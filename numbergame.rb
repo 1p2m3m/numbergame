79d4aa2ceb32eb5f4d0385e86bec5a8ab40f4520
1p2m3m <pmezger7@gmail.com>
2018-02-07 19:56:46 +0100

    3 trys

--- numbergame.rb
+++ numbergame.rb
@@ -5,16 +5,31 @@ name = gets.chomp
 puts "Welcome #{name}"
 
 number = rand(1..10)
+
 puts "Please guess a number between 1 -10"
 
 winner = false
-while winner == false
+
+counter = 3
+while winner == false && counter > 0
   guess = gets.chomp.to_i
 
-if guess > number then
-    puts "Sorry, It's too high"
-  elsif guess < number then
-    puts "Sorry, It's too low"
+  if guess > number
+    if counter > 1
+      puts "Sorry, It's too high"
+      counter-=1
+    else
+      puts "haha you lost"
+      return
+    end
+  elsif guess < number
+    if counter > 1
+      puts "Sorry, It's too low"
+      counter-=1
+    else
+      puts "haha you lost"
+      return
+    end
   else
     puts "#{name} you are correct"
     winner = true
